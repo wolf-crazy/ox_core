b2117ffca1d8594c48277d2d97e689913af10ee8
wolf-crazy <danieljosedacosta16000@gmail.com>
2026-07-16 22:55:41 -0300

    Fix environment variable name for version bump
    Signed-off-by: wolf-crazy <danieljosedacosta16000@gmail.com>

--- .github/actions/bump-package-version.js
+++ .github/actions/bump-package-version.js
@@ -1,6 +1,6 @@
 const packageJson = await Bun.file('./package.json').json();
 
-const newVersion = process.env.TGT_RELEASE_VERSION;
+const newVersion = process.env.TG_RELEASE_VERSION;
 packageJson.version = newVersion.replace('v', '');
 
 await Bun.write('./package.json', JSON.stringify(packageJson, null, 2));
